1ebb8cd1e4f321cdb88f2caa3204ac911760cba4
sonusatish34 <sonusatish34@gmail.com>
2025-01-22 12:30:04 +0530

    w

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,14 @@
 // next-sitemap.js
 
 module.exports = {
-  siteUrl: 'https://www.yourdomain.com', // Replace with your domain
+  siteUrl: 'https://dozzy-admin-website-seven.vercel.app/', // Replace with your domain
   generateRobotsTxt: true, // Optional, generates a robots.txt file
   changefreq: 'daily', // How often pages change
   priority: 0.7, // Default priority
   sitemapSize: 7000, // Limit the number of URLs per sitemap
+  additionalSitemaps: [
+    'https://dozzy-admin-website-seven.vercel.app/sitemap-about.xml', // Additional sitemap 1
+    'https://dozzy-admin-website-seven.vercel.app/sitemap-contact.xml', // Additional sitemap 2
+    // Add more sitemaps here as needed
+  ],
 }
